bd0ae8721e3f26ad5d7343e838f7d2e1308e830e
zhichengSONG <songzhicheng66@gmail.com>
2024-05-20 15:49:04 +0800

    update basic function

--- src/environment.py
+++ src/environment.py
@@ -1,7 +1,7 @@
 import math
 
 class Car:
-    def __init__(self,car_type):
+    def __init__(self,car_type, time_step):
         self.type = car_type        # 0: ego vehicle; 1: surrounding
         self.pos_x = 0
         self.pos_y = 0
@@ -13,9 +13,30 @@ class Car:
         self.omega = 0
         self.traj_x = []
         self.traj_y = []
+        self.traj_phi = []
         self.center1_pos = []
         self.center2_pos = []
+        self.dt = time_step
         self.r = 1.2
+
+    def step(self):
+        self.pos_x += self.v_x * self.dt
+        self.pos_y += self.v_y * self.dt
+        self.v_x += self.a_x * self.dt
+        self.v_y += self.a_y * self.dt
+        self.phi += self.omega * self.dt 
+
+    def updateTrajectoryPhi(self):
+        self.traj_phi[0] = self.phi
+        for i in range(len(self.traj_x) - 1):
+            self.traj_phi[i + 1] = math.atan2((self.traj_y[i+1] - self.traj_y[i])/self.dt, (self.traj_x[i+1]-self.traj_x[i])/self.dt)
+
+    def updateCenterPos(self):
+        for i in range(len(self.traj_x)):
+            self.center1_pos[i][0] = self.traj_x[i] + self.r * math.sin(self.traj_phi[i])
+            self.center2_pos[i][0] = self.traj_x[i] - self.r * math.sin(self.traj_phi[i])
+            self.center1_pos[i][1] = self.traj_y[i] + self.r * math.cos(self.traj_phi[i])
+            self.center2_pos[i][1] = self.traj_y[i] - self.r * math.cos(self.traj_phi[i])
 
 class Lane:
     def __init__(self, num_of_lane, line_width):
@@ -32,7 +53,7 @@ class Env:
 
     def collisionCheckForAllCar(self):
         for car in self.surrounding_vehicle_list:
-            for i in range(len(self.ego_vehicle.traj)):
+            for i in range(len(self.ego_vehicle.traj_x)):
                 if self.collisionCheck(self.ego_vehicle, car, i):
                     return True         # collision detected
         return False                    # no collision
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from polynomial_trajectory_planner import Polynomial
 from polynomial_trajectory_planner import Sampler
 from environment import Car
 from environment import Lane
+from environment import Env
 import random
 
 # initialize cars
@@ -24,6 +25,9 @@ ego_car.v_x = 6
 ego_car.pos_x = 2
 ego_car.pos_y = 2
 
+# initial Env
+env = Env(ego_car,surrounding_car_list)
+
 # initialize poly for surrounding car and ego car
 poly_for_surrounding_car_list = [] 
 for _ in range(num_of_surrounding_car):
@@ -33,11 +37,21 @@ poly_for_ego_car = Polynomial(3)
 
 # initialize lane and sampler
 lane = Lane(2, 4)
-sampler = Sampler(lane.line_width * lane.num_of_lane - ego_car.r, ego_car.r, 300, 1.5 * ego_car.r, 25, 10, -3, 1.5, poly_for_ego_car.motionTime)
+sampler = Sampler(300, 1.5 * ego_car.r, lane.line_width * lane.num_of_lane - ego_car.r, ego_car.r, 25, 10, -3, 1.5, poly_for_ego_car.motionTime)
 
 
 while True:
+    # generate trajectory for surrounding car
+    for i in range(num_of_surrounding_car):
+        poly_for_surrounding_car_list[i].getTrajectory(surrounding_car_list[i].pos_x, surrounding_car_list[i].ego_car.v_x, 0,\
+                                                        surrounding_car_list[i].pos_x + surrounding_car_list[i].ego_car.v_x * poly_for_surrounding_car_list[i].T, surrounding_car_list[i].ego_car.v_x, 0, 15)
+    # poly for recording best trajectory get by the sampler
+    poly_sample = Polynomial(3)
     # sample for 20 points for each time
     for i in range(20):
         end_pos_x, end_vel_x = sampler.sample_longitude(ego_car.pos_x, ego_car.v_x)
-        poly_for_surrounding_car_list[i].getTrajectory(ego_car.pos_x, ego_car.v_x, ego_car.a_x, end_pos_x, end_vel_x, 0)
+        poly_sample.getTrajectory(ego_car.v_x, ego_car.v_y, ego_car.a_x, end_pos_x, end_vel_x, 0, 15)
+        if not env.collisionCheckForAllCar():
+            poly_sample.fitness = -1
+        else:
+            
